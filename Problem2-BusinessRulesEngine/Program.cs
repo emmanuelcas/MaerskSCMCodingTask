59cdb32aecb99fe2821b92d8c575e62a0652ed7b
Emmanuel <emmanuel.decarvalho@agechecked.com>
2021-11-08 19:27:14 +0000

    Problem 2 Project creation

---
+++ Problem2-BusinessRulesEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem2_BusinessRulesEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
