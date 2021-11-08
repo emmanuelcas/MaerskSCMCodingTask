2ae347722acb3025705aaec8646d79552ed97508
Emmanuel <emmanuel.decarvalho@agechecked.com>
2021-11-08 18:18:15 +0000

    Project Creation

---
+++ Problem1-PromotionEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem1_PromotionEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
